06c697d4f7b02764bdb8dc89891aa61d9265db49
Robert Moegenburg <r.moegenburg@t-online.de>
2023-02-06 20:23:07 +0100

    first commit,addet models and Prefabs

---
+++ PacManii/Assets/Scripts/GhostEnemy.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostEnemy : MonoBehaviour
+{
+    // STATS LEVEL 1  SCATTER 7" CHASE 20" SCATTER 7" CHASE 20" SCATTER 5" CHASE 20" SCATTER 5" 
+    // LEVEL 2-4  - 7 - 20 - 7 - 20 - 5 - 17 - 0
+    // LEVEL 5+   - 5 - 20 - 5 - 20 - 5 - 17 - 0
+    // RED GHOST BLINKY can allways chase when less dots remaining level 1  - 20, 30,40,50
+    /* #############################################
+    // MOVEMENT MECHANIC TARGET 
+    ################################################
+    //1. Top has highest priority, then Left, down , right when same Linear Dist²
+    // LINEAR CALCULATION DIST² =  x² + y² ( 8² + 2²)  = 68
+
+    2. IN SCATTER MODE  PINK GOES TOP LEFT CORNER AND SUROUNDING, BLINKY RED GOES TO RIGHT and SUROUNDING
+    .YELLOW GHOST LEFT BOTTOM CORNER an big surround and BLUE GHOST RIGHT CORNER
+    3. DIFFERENT TARGETING . ROSE GHOST Targeting HIT 2 Steps Right but when PacMan facing up ( Target +4 +2 Left)
+
+    ################################################ POSITION ################################################
+    PACMAN  [40, 38] REDGHOST [48,38], ROSEGHOST [44,42] TURKY[38,52] ORANGEGHOST [50,46]
+    */
+    
+    private enum mode
+    {
+        CHASE,
+        SCATTER,
+        EATEN,
+        FRIGHTENED
+    }
+    private enum Direction
+    {
+        Left,
+        Right,
+        Up,
+        Down
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void EnemyStateChase()
+    {
+        // MAIN STATE
+    }
+    private void EnemyStateScatter()
+    {
+        //TIMEEVENT AFTER CHASE
+    }
+    private void EnemyStateEaten()
+    {
+        //IF PACMAN HIT WHILE FRIGHTENED STATE
+        // new Target GhostHouse, when reached turn back to ChaseMode
+    }
+    private void EnemyStateFrightened()
+    {
+        // IF PACMAN EATS POWER PELLET
+        // TURN 180° 
+        // DIRECTION RANDOM
+    }
+}
---
+++ PacManii/Assets/Scripts/MazeGrid.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGrid 
+{
+  // GRID 21 x 21  DD: Leere Felder ( bewegung möglich)
+  /*
+    BLUE WALLS , Grey Entry, Pellets, Great Pellets4 & Teleport
+    CONDITIONS:
+    BLUEWALLS CANT ENTERED#
+    
+
+  */
+  // CREATE GRID  
+  
+}
