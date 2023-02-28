4a85872a9fc2d2500a3e9fed7e5bb9e25388962d
Robert Moegenburg <r.moegenburg@t-online.de>
2023-02-28 21:40:44 +0100

    anim

--- PacManii/Assets/Scripts/Menu/MenuAnimation.cs
+++ PacManii/Assets/Scripts/Menu/MenuAnimation.cs
@@ -6,7 +6,7 @@ public class MenuAnimation : MonoBehaviour
 {
     private Vector3 startPosition;
     private float maxscrollLenght= 21.0f;
-    private float speed =5.0f;
+    public float speed =5.0f;
     // Update is called once per frame
     private void Start() {
         startPosition=  this.gameObject.transform.position;
